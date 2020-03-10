f8d7128129c0b7e7491b0d43a2ac5963976dc8fe
I-Weng <i.weng024@gmail.com>
2020-03-10 18:04:25 -0400

    Worksheet 03/10

---
+++ advanced_data.py
@@ -0,0 +1,165 @@
+############# Q9. Print the sum of the first and third element in the list "numbers" and print the result ############
+# Topic: tuples
+
+numbers = (4, 9, 16, 36)
+
+# Code here
+
+print(4+9)
+
+############# Q10. Fill out the next 3 nubmers in the pattern using the append() function and print the result ############
+# Topic: lists, list functions
+
+numbers = [4, 9, 16, 25]
+
+# Code here
+
+
+
+
+
+
+
+############# Q11. Append the sum of the 2nd and 4th element in the list "numbers" to itself and print the result ############
+# Topic: lists, list functions
+
+numbers = [4, 9, 16, 25]
+
+# Code here
+
+
+
+
+
+
+############# Q12. Fill in the missing number between 16 and 36 and print the result ############
+# Topic: lists, list functions
+
+numbers = [4, 9, 16, 36]
+
+# Code here
+
+
+
+
+
+############# Q13. Remove the element that breaks the pattern and print the result ############
+# Topic: lists, list functions
+
+numbers = [3, 9, 16, 25, 36]
+
+
+
+
+
+
+
+
+
+############# 
+# Q14. Create a dictionary with the following data and print the dictionary:
+# Alex has $100
+# Kevin has $50
+# Henry has $500
+############
+
+# Topic: dictionaries
+
+# Code here
+
+
+
+
+
+
+
+
+
+
+############# 
+# Q15 A. Create a dictionary with the following transactions and print the dictionary:
+# Alex purchased a sweater for $100
+# Kevin purchased a bookbag for $50
+# Henry purchased a scarf for $500
+############
+
+# Topic: nested dictionaries
+
+# Code here
+
+
+
+
+
+
+
+
+
+
+
+############# Q15 B. Using the dictionary from Q15, add the following transactions and print the dictionary:
+# Jason purchased a wallet for $250
+# Michael purchased a pair of sneakers for $50
+# Andrew purchased a t-shirt for $20 AND a hoodie for $30
+
+# Also, print the value of Andrew's hoodie without using print(30)
+
+### YOU MAY NOT EDIT THE ORIGINAL DICTIONARY
+
+# Topic: nested dictionaries
+
+# Code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+############# Q162. Using the dictionary from Q15 B, MODIFY the following transactions and print the dictionary:
+# Jason's wallet purchase: $100
+# Alex's sweater purchase: $20
+# Henry's scarf purchase: $200
+
+### YOU MAY NOT EDIT THE ORIGINAL DICTIONARY
+
+# Topic: nested dictionaries
+
+# Code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############# Q16. Using the dictionary from Q15, give every purchase a 10% discount and print the dictionary:
+
+### YOU MAY NOT EDIT THE ORIGINAL DICTIONARY
+
+# Topic: nested dictionaries
+
+# Code here
+
+
+
+
+
+
